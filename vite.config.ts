8153cff6a3c52a0dc9ce94adaa2a7b2a3fd915fe
Sam Ollason <sam.ollason@howdeninsurance.co.uk>
2026-01-10 12:43:11 +0000

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/kraken-api-explorer/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
